92ad93e79679da2b6df7dd0cdd967f3ee4a26025
Stenio <steniohallowedbethyname@gmail.com>
2023-09-11 14:35:52 -0300

    primeiro commit do projeto da rocketseat

---
+++ html/script.js
@@ -0,0 +1,27 @@
+function toggleNode() {
+    const html = document.documentElement
+
+    //if (html.classList.contains('light')) {
+        //html.classList.remove('light')
+    //} else { 
+        //html.classList.add
+    //}
+
+    html.classList.toggle('light')
+
+
+    //substituir a imagem aproveitando a lógica da mudança para o light
+   /*const img = document.querySelector("#profile img") 
+        if (html.classList.contains('light')) {
+            img.setAttribute("src", "/caminhdodaimagem.png")
+
+        } else {
+            img.setAttribute("src", "caminhodaimagempadrao.png")
+        }
+
+   }
+   
+   */
+
+
+}
